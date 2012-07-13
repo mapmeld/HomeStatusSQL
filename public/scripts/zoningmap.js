0f0d592c83b5a2476afd38b554dfe0a2c25cc22a
Nick Doiron <ndoiron@mapmeld.com>
2012-07-12 22:43:45 -0700

    styling and link to housing record

--- public/scripts/zoningmap.js
+++ public/scripts/zoningmap.js
@@ -80,7 +80,7 @@ function mapReport(report){
       }
       else{
         //for(var i=0;i<b.rows.length;i++){
-		  iContent += "Found.<br/>";
+		  iContent += "<a href='/statusone.html?address=" + b.rows[0].key + "'>Found</a>.<br/>";
         //}
       }
       infoWindow.setContent( infoWindow.getContent() + iContent);
