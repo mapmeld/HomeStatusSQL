9f9777cacc27673e35ef5965534c3657d5820206
Nick Doiron <ndoiron@mapmeld.com>
2012-07-13 14:05:26 -0700

    link scf map to combined map

--- public/scripts/zoningmap.js
+++ public/scripts/zoningmap.js
@@ -80,7 +80,7 @@ function mapReport(report){
       }
       else{
         //for(var i=0;i<b.rows.length;i++){
-		  iContent += "<a href='/statusone.html?id=" + b.rows[0].key + "'>Found record</a>.<br/>";
+		  iContent += "<a href='/statuscombine.html?id=" + b.rows[0].key + "'>Found record</a>.<br/>";
         //}
       }
       infoWindow.setContent( infoWindow.getContent() + iContent);
