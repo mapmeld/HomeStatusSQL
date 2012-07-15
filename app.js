98d40e88dc36b810d8d0487dd8fb2e91ed205fdf
Nick Doiron <ndoiron@mapmeld.com>
2012-07-14 21:03:20 -0700

    test no results conversion to geo search

--- app.js
+++ app.js
@@ -191,7 +191,7 @@ var init = exports.init = function (config) {
   // Code Enforcement cases: look up by geo
   app.get('/osmgeo', function(req, res){
     // OSM Nominatim search
-    var sendurl = 'http://nominatim.openstreetmap.org/search?format=json&q=' + encodeURIComponent(req.query["streetname"]) + "&maxlat=33.0363&maxlon=-83.2819&minlat=32.5745&minlon=-84.10583";
+    var sendurl = 'http://nominatim.openstreetmap.org/search?format=json&q=' + encodeURIComponent(req.query["streetname"]) + "+Macon&maxlat=33.0363&maxlon=-83.2819&minlat=32.5745&minlon=-84.10583";
     /* if(sendurl.toLowerCase().indexOf("macon") == -1){
       sendurl += ", Macon, Bibb County, GA";
     } */
