28991241ec5757e16deaa4e3ed3eee18e4407bae
Nick Doiron <ndoiron@mapmeld.com>
2012-07-15 18:34:43 -0700

    attempt kml api

--- app.js
+++ app.js
@@ -173,7 +173,7 @@ var init = exports.init = function (config) {
 		var kmlplacemarks = '';
 		for(var pt=0;pt<body.rows.length;pt++){
 		  kmlplacemarks += '		<Placemark>\n			<name>' + body.rows[pt].value.address + '</name>\n			<address>' + body.rows[pt].value.address + '</address>\n';
-		  kmlplacemarks += '			<description><![CDATA[<div class="googft-info-window" style="font-family:sans-serif">\n<b>Address:</b>' + body.rows[pt].value.address + '<br>\n<b>Case ID:</b> ' + body.rows[pt].value.ecd_id + '<br>\n<b>Opened:</b> ' + body.rows[pt].value.opendate + '<br>\n<b>Closed:</b> ' + body.rows[pt].value.closedate + '<br>\n<b>Inspector:</b> ' + body.rows[pt].value.inspector + '<br>\n<b>Cause:</b> ' + body.rows[pt].value.cause + '<br>\n<b>Neighborhood:</b> ' + body.rows[pt].value.address || body.rows[pt].value.neighborhood + '<br>\n</div>]]></description>\n';
+		  kmlplacemarks += '			<description><![CDATA[<div class="googft-info-window" style="font-family:sans-serif"><b>Address:</b>' + body.rows[pt].value.address + '<br><b>Case ID:</b> ' + body.rows[pt].value.ecd_id + '<br><b>Opened:</b> ' + body.rows[pt].value.opendate + '<br><b>Closed:</b> ' + body.rows[pt].value.closedate + '<br><b>Inspector:</b> ' + body.rows[pt].value.inspector + '<br><b>Cause:</b> ' + body.rows[pt].value.cause + '<br><b>Neighborhood:</b> ' + body.rows[pt].value.address || body.rows[pt].value.neighborhood + '<br></div>]]></description>\n';
 		  kmlplacemarks += '			<styleUrl>#BasicStyle</styleUrl>\n			<ExtendedData>\n				<Data name="F">\n					<value>F</value>\n				</Data>\n			</ExtendedData>\n';
 		  kmlplacemarks += '			<Point>\n				<coordinates>' + body.rows[pt].value.loc[1] + ',' + body.rows[pt].value.loc[0] + ',0</coordinates>\n			</Point>\n		</Placemark>\n';
 		}
